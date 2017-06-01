a56570b4b4fe99516f2eb79968c3f410cf07e3bd
Michael Pesce <Michael Pesce>
2017-06-01 18:38:21 -0400

    added switch case argument to recognize the command entered

--- liri.js
+++ liri.js
@@ -1,4 +1,26 @@
 var twitter = require("twitter");
 var request = requires("request");
 var spotify = requires("spotify");
+var OMDB = requires("OMDB");
 
+var command = process.argv[2];
+
+switch (command) {
+
+	case "my-tweets":
+		tweet.getTweets();
+		break;
+
+	case "spotify-this-song":
+		song.getSong();
+		break;
+
+	case "movie-this":
+		movie.getMovie();
+		break;
+
+	case "do-what-it-says":
+		say.doIt();
+		break;
+
+}
